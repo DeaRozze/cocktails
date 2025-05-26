5d25f27f09f268c22ded0cc66e5146922af87497
DeaRozze <vl4dkudi@gmail.com>
2025-05-26 10:00:09 +0300

    поменял путь картинки

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -18,7 +18,7 @@ function removeIngredient() {
 </script>
 
 <template>
-  <ApplayOut imgUrl='../src/assets/img/bg-1.jpg' :backFunc='removeIngredient' :is-back-button-visible="!!ingredient">
+  <ApplayOut imgUrl='src/assets/img/bg-1.jpg' :backFunc='removeIngredient' :is-back-button-visible="!!ingredient">
     <div class="wrapper">
       <div v-if="!ingredient || !cocktails" class="info">
         <div class="title">Choose your drink</div>
